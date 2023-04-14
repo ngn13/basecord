62c08eb32596baf6d9dc495f4d397a8904387699
ngn <ngn13proton@proton.me>
2023-04-14 22:00:40 +0300

    fixing it fr this time

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
         author="ngn13",
         description=DESCRIPTION,
         packages=find_packages(),
-        install_requires=["nextcord", "zipfile", "colorama"], 
+        install_requires=["nextcord", "colorama"], 
         entry_points={"console_scripts": ["basecord=basecord.build:main"]}
 )
